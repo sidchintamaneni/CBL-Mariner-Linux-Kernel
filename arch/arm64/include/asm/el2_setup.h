04a8ec5c8e412c9d32e175d1c3da01c09c83bc3c
Jeff Kirsher <jeffkirsher@microsoft.com>
2026-03-30 09:30:55 -0700

    13-cobalt-added-override-for-mdir

--- arch/arm64/include/asm/el2_setup.h
+++ arch/arm64/include/asm/el2_setup.h
@@ -250,6 +250,11 @@
 .macro __init_el2_nvhe_idregs
 	mrs	x0, midr_el1
 	mrs	x1, mpidr_el1
+	ldr	x2, =0x6D0FD490
+	cmp	x0, x2
+	bne	.Loverride_cobalt_\@
+	ldr	x0, =0x410FD490
+.Loverride_cobalt_\@:
 	msr	vpidr_el2, x0
 	msr	vmpidr_el2, x1
 .endm
--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -3002,6 +3002,8 @@ static u64 __ro_after_init boot_cpu_aidr_val;
 static void init_imp_id_regs(void)
 {
 	boot_cpu_midr_val = read_sysreg(midr_el1);
+	if( boot_cpu_midr_val == 0x6D0FD490)
+		boot_cpu_midr_val = 0x410FD490;
 	boot_cpu_revidr_val = read_sysreg(revidr_el1);
 	boot_cpu_aidr_val = read_sysreg(aidr_el1);
 }
