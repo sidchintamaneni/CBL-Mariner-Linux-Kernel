ce5905b90a5c94872a9905d3d52b9fea5d4271ae
Jeff Kirsher <jeffkirsher@microsoft.com>
2026-07-10 18:37:30 +0000

    ACPI: CPPC: Read FFH performance counters together
    Read the delivered and reference AMU counters in a single synchronous
    call on the target CPU. This reduces sampling skew and IPI overhead when
    CPPC describes both performance counters as Fixed Hardware registers.
    
    Keep the generic cpc_read() behavior for counters in other ACPI address
    spaces and initialize the paired outputs before invoking the architecture
    hook so failures cannot expose uninitialized values.
    
    Signed-off-by: Jeff Kirsher <jeffkirsher@microsoft.com>
    Signed-off-by: Siddharth Chintamaneni <sidchintamaneni@gmail.com>

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -437,6 +437,15 @@ static void cpu_read_constcnt(void *val)
 		      0UL : read_constcnt();
 }
 
+static void cpu_read_cnt_pair(void *val)
+{
+	u64 *regs = val;
+
+	regs[0] = read_corecnt();
+	regs[1] = this_cpu_has_cap(ARM64_WORKAROUND_2457168) ?
+		  0UL : read_constcnt();
+}
+
 static inline
 int counters_read_on_cpu(int cpu, smp_call_func_t func, u64 *val)
 {
@@ -499,6 +508,28 @@ int cpc_read_ffh(int cpu, struct cpc_reg *reg, u64 *val)
 	}
 
 	return ret;
+}
+
+int cpc_read_ffh_perf_pair(int cpu, struct cpc_reg *reg,
+			   struct cpc_reg *reg2, u64 *val)
+{
+	int ret;
+
+	if ((u64)reg->address != 0x0 || (u64)reg2->address != 0x1)
+		return -EOPNOTSUPP;
+
+	ret = counters_read_on_cpu(cpu, cpu_read_cnt_pair, val);
+	if (ret)
+		return ret;
+
+	val[0] &= GENMASK_ULL(reg->bit_offset + reg->bit_width - 1,
+			      reg->bit_offset);
+	val[0] >>= reg->bit_offset;
+	val[1] &= GENMASK_ULL(reg2->bit_offset + reg2->bit_width - 1,
+			      reg2->bit_offset);
+	val[1] >>= reg2->bit_offset;
+
+	return 0;
 }
 
 int cpc_write_ffh(int cpunum, struct cpc_reg *reg, u64 val)
